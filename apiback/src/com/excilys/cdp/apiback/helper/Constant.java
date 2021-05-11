86e15d9a4d2655495b6b4601ccb5f6ea3efec991
Thuy Dung NGUYEN <thuydungnguyen275@gmail.com>
2021-05-11 15:52:14 +0200

    update mysql driver

--- apiback/src/com/excilys/cdp/apiback/helper/Constant.java
+++ apiback/src/com/excilys/cdp/apiback/helper/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 	
 	public static final String URL = "jdbc:mysql://localhost:3306/";
 	public static final String DB_NAME = "computer-database-db";
-	public static final String DRIVER = "com.mysql.jdbc.Driver";
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	public static final String USERNAME = "admincdb";
 	public static final String PASSWORD = "qwerty1234";
 	
